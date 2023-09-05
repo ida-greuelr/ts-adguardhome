a424379ee36dfdae4747d1d677ce7d0f142365e1
Rene Greuel <rene.greuel@ida-sds.com>
2023-09-05 11:08:55 +0200

    Added more logging on startup and changed config url handling

--- startAdGuardHome.sh
+++ startAdGuardHome.sh
@@ -12,11 +12,25 @@ function echo_fail() {
   echo -e "\n\e[0;31m 🛑 \e[1;31m$1\n\e[0m"
 }
 
+function check_result() {
+  if [ $1 -eq 0 ]; then
+      echo_success "DONE"
+    else
+      echo_fail "FAILED"
+      exit 1
+  fi
+}
+
+echo "Starting AdGuardHome with following environment:"
+set | grep AG_
+echo ""
+
 if [ -n "${AG_CONFIG_URL}" ] && [ -n "${AG_USER}" ] && [ -n "${AG_HTPASSWD}" ]; then
 
-  echo_warning "Fetching AdGuardHome config. from ${AG_CONFIG_URL}/AdGuardHome.yaml"
-  curl -L "${AG_CONFIG_URL}/AdGuardHome.yaml" > /opt/AdGuardHome/AdGuardHome.yaml
-
+  echo_warning "Fetching AdGuardHome config. from ${AG_CONFIG_URL}"
+  curl -L "${AG_CONFIG_URL}" > /opt/AdGuardHome/AdGuardHome.yaml
+  check_result $?
+  
   echo_warning "Updating local config"
   export CURRENT_HOST=$(tailscale status --peers=false --json | jq -r '.CertDomains[0]')
   sed -i "s/ADMINUSER/${AG_USER}/" /opt/AdGuardHome/AdGuardHome.yaml
